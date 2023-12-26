835e60553d0efc053c447a398119f62c3ccd779c
origin <mambo771@mail.ru>
2023-12-26 19:30:12 +0300

    Initial commit

--- src/ObjectandMethods/Main.java
+++ src/ObjectandMethods/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Authors gogol = new Authors("Николай", "Гоголь");
         Authors tolstoy = new Authors("Лев", "Толстой");
-        Book deadSouls = new Book("Мертвые души", gogol, 1843);
-        Book warAndPeace = new Book("Война и мир", tolstoy, 1863);
+        Book deadSouls = new Book("Мертвые души", gogol, 1842);
+        Book warAndPeace = new Book("Война и мир", tolstoy, 1865);
         System.out.println(deadSouls.getNameBook() + " " + deadSouls.getAuthors().getFirstName()+ " " + deadSouls.getAuthors().getSecondName() + " " + deadSouls.getYearPublisher() + " год " );
         deadSouls.setYearPublisher(1845);
         System.out.println(deadSouls.getNameBook() + " " + deadSouls.getAuthors().getFirstName()+ " " + deadSouls.getAuthors().getSecondName() + " " + deadSouls.getYearPublisher() + " год " );
         System.out.println(warAndPeace.getNameBook() + " " + warAndPeace.getAuthors().getFirstName()+ " " + warAndPeace.getAuthors().getSecondName() + " " + warAndPeace.getYearPublisher() + " год " );
-        warAndPeace.setYearPublisher(1875);
+        warAndPeace.setYearPublisher(1876);
         System.out.println(warAndPeace.getNameBook() + " " + warAndPeace.getAuthors().getFirstName()+ " " + warAndPeace.getAuthors().getSecondName() + " " + warAndPeace.getYearPublisher() + " год " );
     }
 }
