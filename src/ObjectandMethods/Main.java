32df5897d5076f842c71ae96257d8c5ef090db5d
origin <mambo771@mail.ru>
2023-12-26 19:17:51 +0300

    Initial commit

--- src/ObjectandMethods/Main.java
+++ src/ObjectandMethods/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Authors gogol = new Authors("Николай", "Гоголь");
         Authors tolstoy = new Authors("Лев", "Толстой");
-        Book deadSouls = new Book("Мертвые души", gogol, 1842);
-        Book warAndPeace = new Book("Война и мир", tolstoy, 1865);
+        Book deadSouls = new Book("Мертвые души", gogol, 1843);
+        Book warAndPeace = new Book("Война и мир", tolstoy, 1863);
         System.out.println(deadSouls.getNameBook() + " " + deadSouls.getAuthors().getFirstName()+ " " + deadSouls.getAuthors().getSecondName() + " " + deadSouls.getYearPublisher() + " год " );
         deadSouls.setYearPublisher(1845);
         System.out.println(deadSouls.getNameBook() + " " + deadSouls.getAuthors().getFirstName()+ " " + deadSouls.getAuthors().getSecondName() + " " + deadSouls.getYearPublisher() + " год " );
